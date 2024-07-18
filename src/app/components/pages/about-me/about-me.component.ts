4d898760e557998073e485c608261d01e857e789
griseldaalegre <alegregriselda5@gmail.com>
2024-07-18 18:11:23 -0300

    Modificacion de estilos

--- src/app/components/pages/about-me/about-me.component.ts
+++ src/app/components/pages/about-me/about-me.component.ts
@@ -9,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.component.css'
 })
 export class AboutMeComponent {
-  
+  formations: any[] = [
+    {
+      title: "Tecnicatura Universitaria en Desarrollo de Aplicaciones Informáticas",
+      description: "2023-Actualidad. Universidad Nacional del Centro."
+    },
+    {
+      title: "Seminario de Angular",
+      description: "2024. Universidad Nacional del Centro."
+    },
+    {
+      title: "Fundamentos de Angular",
+      description: "2024. Crehana."
+    },
+    {
+      title: "Software Tester QA",
+      description: "2022. Educación IT."
+    },
+    {
+      title: "Curso de Análisis y Diseño de Casos de Pruebas",
+      description: "2022. Educación IT."
+    },
+    {
+      title: "Programador Web",
+      description: "Centro de Formación profesional n 403."
+    },
+    {
+      title: "Programador full stack",
+      description: "Programa Oracle Next Education, Alura Latam."
+    },
+    {
+      title: "Curso ...",
+      description: "2022. Educación IT."
+    }
+  ];
+
 }
